71d0f8d74f657314061890bba0f1239594e06052
Adolfo Rodriguez Tsouroukdissian <adolfo.rodriguez@pal-robotics.com>
2012-12-04 17:22:51 +0000

    Add examples for joint-space motion planning (left- and right-arm), and task-space motion planning (right-arm). Refs #4677.
    git-svn-id: svn+ssh://server/srv/svn/repos/branches/groovy_migration/pal-ros-pkg/stacks/reem_tutorials@37808 5e370ff8-3418-0410-babe-3378cc20a00d

--- src/init_pose.cpp
+++ src/init_pose.cpp
@@ -1,9 +1,44 @@
+/*
+ * Software License Agreement (Modified BSD License)
+ *
+ *  Copyright (c) 2012, PAL Robotics, S.L.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of PAL Robotics, S.L. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** \author Adolfo Rodriguez Tsouroukdissian. */
+// NOTE: The contents of this file are based on the JointTrajectoryAction tutorial available here:
+// http://www.ros.org/wiki/pr2_controllers/Tutorials/Moving%20the%20arm%20using%20the%20Joint%20Trajectory%20Action
+
 #include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
 #include <control_msgs/FollowJointTrajectoryAction.h>
-
-namespace reem_tutorials
-{
 
 class MoveGroup
 {
@@ -91,8 +126,6 @@ bool MoveGroup::sendGoal(const std::vector<double>& pose, const ros::Duration& d
   return true;
 }
 
-} // reem_tutorials
-
 int main(int argc, char** argv)
 {
   // Init the ROS node
@@ -107,9 +140,9 @@ int main(int argc, char** argv)
   }
 
   // Instances that will move each joint group to the desired pose
-  reem_tutorials::MoveGroup right_arm_torso("right_arm_torso_controller");
-  reem_tutorials::MoveGroup left_arm("left_arm_controller");
-  reem_tutorials::MoveGroup head("head_traj_controller");
+  MoveGroup right_arm_torso("right_arm_torso_controller");
+  MoveGroup left_arm("left_arm_controller");
+  MoveGroup head("head_traj_controller");
 
   // Desired poses for each joint group
   std::vector<double> right_arm_torso_pose(right_arm_torso.getJointNames().size(), 0.0);
