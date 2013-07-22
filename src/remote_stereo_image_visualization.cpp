8236b3674f9eee89b3e92b8494d88977aecda021
Jordi Pages <jordi.pages@pal-robotics.com>
2013-07-22 09:55:37 +0000

    fixes #6258. The timeout allowed before throwing an exception has been increased
    git-svn-id: svn+ssh://server/srv/svn/repos/branches/OROCOS_2.X/pal-ros-pkg/stacks/reem_tutorials@43552 5e370ff8-3418-0410-babe-3378cc20a00d

--- src/remote_stereo_image_visualization.cpp
+++ src/remote_stereo_image_visualization.cpp
@@ -85,9 +85,9 @@ void printCalibrationData(const std::string& eye, const sensor_msgs::CameraInfo&
 int main(int argc, char** argv)
 {
   // Init the ROS node
-  ros::init(argc, argv, "remove_monocular_image_visualization");
+  ros::init(argc, argv, "remove_stereo_image_visualization");
 
-  ROS_INFO("Starting remove_monocular_image_visualization application ...");
+  ROS_INFO("Starting remove_stereo_image_visualization application ...");
 
   // Precondition: Valid clock
   ros::NodeHandle nh;
@@ -104,7 +104,7 @@ int main(int argc, char** argv)
                                     "/stereo/right/image",                 //right image topic name
                                     pal::StereoCameraClient::JPEG,         //image transport type. Choose between RAW (uncompressed) and JPEG
                                     pal::StereoCameraClient::APPROX_TIME,  //topics synchronization policy
-                                    1.0,                                   //timeout in seconds attempting to get data from topics
+                                    5.0,                                   //timeout in seconds attempting to get data from topics
                                     static_cast<float>(frequency*2),       //max rate at which images may be polled
                                     "/stereo/left/camera_info",            //left camera info topic name
                                     "/stereo/right/camera_info");          //right camera info topic name
